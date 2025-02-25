ec576b36ad38c47813dc9751d7499ce60493d7ef
Roberto <montanes.rarob24@triana.salesianos.edu>
2025-02-25 20:37:21 +0100

    Tetris

--- pruebas/index.js
+++
@@ -1,8 +0,0 @@
-const numero_clicks = document.getElementById('numero-click');
-let contador = 0;
-const boton = document.getElementById('circulo')
-boton.addEventListener('click', function(){
-    contador++;
-    numero_clicks.textContent = contador;
-    
-})
